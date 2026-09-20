5443b8e39d8044ed79a357072ee3fc7bafea4258
MSreekari <msreekari11@gmail.com>
2026-09-20 16:40:40 +0530

    Added both folders

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,5 @@
-import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Applied RAG Security using ZTA"
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "super-secret-zero-trust-hmac-sha256-key"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    GROQ_API_KEY: str = "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"
+    GROQ_API_KEY: str
     CHROMA_PERSIST_DIR: str = "./data/chroma_db"
 
     model_config = SettingsConfigDict(
@@ -15,5 +15,6 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
         extra="ignore"
     )
+
 
 settings = Settings()
